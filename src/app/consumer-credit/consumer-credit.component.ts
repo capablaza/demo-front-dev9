e4c7994d9abac3c4da2c391f309f4314b236c436
carlos apablaza <c.apablaza.z@gmail.com>
2021-08-17 10:28:40 -0400

    test pipeline for check error

--- src/app/consumer-credit/consumer-credit.component.ts
+++ src/app/consumer-credit/consumer-credit.component.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { Credit } from './credit';
-import { SimulatorService } from './simulator.service';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { Credit } from "./credit";
+import { SimulatorService } from "./simulator.service";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-consumer-credit',
-  templateUrl: './consumer-credit.component.html',
-  styleUrls: ['./consumer-credit.component.css'],
+  selector: "app-consumer-credit",
+  templateUrl: "./consumer-credit.component.html",
+  styleUrls: ["./consumer-credit.component.css"],
 })
 export class ConsumerCreditComponent implements OnInit {
-  form;  
+  form;
 
-  constructor(private fb: FormBuilder, private service: SimulatorService, private _snackBar: MatSnackBar) {
+  constructor(
+    private fb: FormBuilder,
+    private service: SimulatorService,
+    private _snackBar: MatSnackBar
+  ) {
     this.form = fb.group({
-      name: ['', Validators.required],
-      email: ['', Validators.required],
-      amount: ['', Validators.required],
-      quotas: ['', Validators.required],
-      incomes: ['', Validators.required],
+      name: ["", Validators.required],
+      email: ["", Validators.required],
+      amount: ["", Validators.required],
+      quotas: ["", Validators.required],
+      incomes: ["", Validators.required],
     });
   }
 
@@ -31,23 +35,22 @@ export class ConsumerCreditComponent implements OnInit {
       amount: this.form.value.amount,
       quotas: this.form.value.quotas,
       income: this.form.value.incomes,
-      isValid: false
+      isValid: false,
     };
-    
-    this.service.evaluateCredit(creditToSimulate).subscribe(credit => {      
-      var message = ""
-      var action = ""
-      
-      if(credit.isValid == true){
-        action = "Aprobado"
-        message = "Felicidades usted cumple los requisitos."
-      }else {
-        action = "Rechazado"
-        message = "Lo lamentamos pero usted no cumple los requisitos."
+
+    this.service.evaluateCredit(creditToSimulate).subscribe((credit) => {
+      var message = "";
+      var action = "";
+
+      if (credit.isValidooo == true) {
+        action = "Aprobado";
+        message = "Felicidades usted cumple los requisitos.";
+      } else {
+        action = "Rechazado";
+        message = "Lo lamentamos pero usted no cumple los requisitos.";
       }
-      
-      this._snackBar.open(message,action, { duration: 5000 });
-      
-    });    
+
+      this._snackBar.open(message, action, { duration: 5000 });
+    });
   }
 }
