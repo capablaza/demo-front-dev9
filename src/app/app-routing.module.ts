af18ad5eeb601c3aaa642dec0c17ca1576a0d430
carlos apablaza <c.apablaza.z@gmail.com>
2021-08-17 10:18:04 -0400

    add form on credit simulator

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-//import { ConsumerCreditComponent } from './consumer-credit/consumer-credit.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ConsumerCreditComponent } from "./consumer-credit/consumer-credit.component";
 //import { MortgageCreditComponent } from './mortgage-credit/mortgage-credit.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
-import { SelectCreditComponent } from './select-credit/select-credit.component';
-import { WelcomeComponent } from './welcome/welcome.component';
+import { PagenotfoundComponent } from "./pagenotfound/pagenotfound.component";
+import { SelectCreditComponent } from "./select-credit/select-credit.component";
+import { WelcomeComponent } from "./welcome/welcome.component";
 
-const routes: Routes = [  
-  {path: '', component : WelcomeComponent},
-  {path: 'select', component : SelectCreditComponent},
-  //{path: 'consumer', component : ConsumerCreditComponent},
+const routes: Routes = [
+  { path: "", component: WelcomeComponent },
+  { path: "select", component: SelectCreditComponent },
+  { path: "consumer", component: ConsumerCreditComponent },
   //{path: 'mortgage', component : MortgageCreditComponent},
-  {path: '**', component: PagenotfoundComponent} 
+  { path: "**", component: PagenotfoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
